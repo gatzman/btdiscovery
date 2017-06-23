5bcd70a4ae5c9db729f03af2a95c581172cb5fb5
cgatzen <gatzen@crossmedia.de>
2017-06-23 14:57:53 +0200

    Initial

--- btdiscover.js
+++ btdiscover.js
@@ -1,7 +1,19 @@
-$(function() {
+$(function () {
     console.log('I am ready');
-    $('.discover').on('click', function() {
+    $('.discover').on('click', function () {
         console.log('Clicked Discover');
 
+
+        options.acceptAllDevices = true;
+
+        console.log('Requesting Bluetooth Device...');
+        navigator.bluetooth.requestDevice(options).then(device => {console.log('> Name:             ' + device.name);
+        console.log('> Id:               ' + device.id);
+        console.log('> Connected:        ' + device.gatt.connected);
+    }).
+        catch(error => {
+            console.log('Argh! ' + error);
+    })
+        ;
     })
 });
